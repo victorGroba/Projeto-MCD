a7d5a1ce12d2eed0ec8a83e8588a39a1c9b0d54b
victorGroba <victorgroba2@gmail.com>
2026-02-23 11:19:38 -0300

    última atualização feita localmente

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vision: {
+          dark: "#0F1535",
+          card: "#1F2749",
+          primary: "#0075FF",
+          secondary: "#A0AEC0",
+          success: "#01B574",
+          warning: "#FFB547",
+          danger: "#E31A1A",
+        }
+      },
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', 'sans-serif'],
+      }
+    },
   },
-};
+  plugins: [],
+}
